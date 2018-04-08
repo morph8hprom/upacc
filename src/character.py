3ca7b01ae9a92e3e824fbb63a322fd84fad4f14b
Noved <morph8hprom@gmail.com>
2018-04-08 16:28:29 -0500

    Updated class definitions

--- src/character.py
+++ src/character.py
@@ -4,4 +4,10 @@
 File used to define class for non player character
 """
 class Character():
-    def __init__(name, desc, attributes = {}, equipment = {})
+    def __init__(name, desc, attributes = {}, equipment = {}):
+        self.name = name
+        self.desc = desc
+        self.attributes = attributes
+        self.equipment = equipment
+
+    
