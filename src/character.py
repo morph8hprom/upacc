c3353bdff2ba2b48ba1bbffec863965cddf91ed1
Noved <morph8hprom@gmail.com>
2018-05-08 11:34:02 -0500

    Updated character parameters:

--- src/character.py
+++ src/character.py
@@ -4,10 +4,9 @@
 File used to define class for non player character
 """
 class Character():
-    def __init__(name, desc, attributes = {}, equipment = {}):
+    def __init__(name, desc, attributes = {}, armor, weapons):
         self.name = name
         self.desc = desc
         self.attributes = attributes
-        self.equipment = equipment
-
-    
+        self.armor = armor
+        self.weapons = weapons
