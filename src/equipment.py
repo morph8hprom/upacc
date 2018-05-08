64426c896bfc2c8b2848f5c8d3347a52587a33a7
Noved <morph8hprom@gmail.com>
2018-05-08 00:42:38 -0500

    Updated equipment methods

--- src/equipment.py
+++ src/equipment.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-File used to define equipment class and related methods
+File used to define equipment class, armor and weapon subclasses,
+and related methods
 """
 
 class Equipment:
@@ -10,12 +11,26 @@ class Equipment:
     """
 
     def __init__(self):
-        self.slots = {'head' : None, 'torso' : None, 'arms' : None,
-                     'legs' : None}
+        self.slots = {}
+
     def __iter__(self):
         # Defines item as an iterable
         return iter(self.slots.items())
 
     def __len__(self):
         return len(self.slots)
-    
+
+    def __contains__(self, item):
+        # Overwrites __contains__ method for easy use of 'if x in items'
+        return item in self.slots.values()
+
+    def __getitem__(self, item):
+        # Overwrites __getitem__ method for easy access to individual items
+        return self.slots[item.slot]
+
+
+    def _equip_item(self, item):
+        # Checks to make sure that the slot attribute of the item
+        # is a valid available slot
+        if item.slot in self.slots.keys():
+            self.slots[item.slot] = item
