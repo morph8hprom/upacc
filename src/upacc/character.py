ad0f44e9e4cd78de207e8290dac99d76dd539df8
Noved <morph8hprom@gmail.com>
2018-05-10 22:40:34 -0500

    Merged character and player files

--- src/upacc/character.py
+++ src/upacc/character.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 """
-File used to define class for non player character
+File used to define Character class and Player and Nonplayer subclasses
 """
+
 class Character():
-    def __init__(name, desc, attributes = {}, armor, weapons):
+    def __init__(self, name, desc, attributes, armor, weapons):
         self.name = name
         self.desc = desc
         self.attributes = attributes
         self.armor = armor
         self.weapons = weapons
+
+
+
+class Player(Character):
+    def __init__(self, name, desc, attributes, armor, weapons):
+        super().__init__(name, desc, attributes, armor, weapons)
+
     
