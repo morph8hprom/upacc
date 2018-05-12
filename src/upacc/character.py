ac652ba1d841aa21fe05c255b341549d5d6dcb7e
Noved <morph8hprom@gmail.com>
2018-05-12 13:08:52 -0500

    Added build_character function

--- src/upacc/character.py
+++ src/upacc/character.py
@@ -18,4 +18,21 @@ class Player(Character):
     def __init__(self, name, desc, attributes, armor, weapons):
         super().__init__(name, desc, attributes, armor, weapons)
 
-    
+class Nonplayer(Character):
+    def __init__(self, name, desc, attributes, armor, weapons):
+        super().__init__(name, desc, attributes, armor, weapons)
+
+def build_character(id):
+    """
+    Gathers character data from json file and returns Character instance.
+    """
+
+    jsontext = resource_string(__name__, 'data/char{}.json'.format(id))
+    d = json.loads(jsontext.decode('utf-8'))
+    d['id'] = id
+    if d['player'] == False:
+        char = Nonplayer(**d)
+    elif d['player'] == True:
+        char = Player(**d)
+    else:
+        raise ValueError('player key must be True or False')
